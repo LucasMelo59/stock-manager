f237c07ee84d5bd101f6a1c76ef30817a00383b1
david heleno <davidheleno@edu.unifor.br>
2025-04-22 18:34:10 -0300

    utilizando strategy

--- src/main/java/br/com/logitrack/stock_flow/service/strategy/UserStrategy.java
+++ src/main/java/br/com/logitrack/stock_flow/service/strategy/UserStrategy.java
@@ -3,7 +3,7 @@ package br.com.logitrack.stock_flow.service.strategy;
 import br.com.logitrack.stock_flow.entity.User;
 import br.com.logitrack.stock_flow.form.UserForm;
 
-public interface userStrategy {
+public interface UserStrategy {
 
     User registerUser(UserForm form);
     User consultUser(Long id);
---
+++ src/main/java/br/com/logitrack/stock_flow/service/strategy/service/UserPFService.java
@@ -0,0 +1,19 @@
+package br.com.logitrack.stock_flow.service.strategy.service;
+
+import br.com.logitrack.stock_flow.entity.User;
+import br.com.logitrack.stock_flow.form.UserForm;
+import br.com.logitrack.stock_flow.service.strategy.UserStrategy;
+
+public class UserPFService implements UserStrategy {
+
+    @Override
+    public User registerUser(UserForm form) {
+        return null;
+    }
+
+    @Override
+    public User consultUser(Long id) {
+        return null;
+    }
+
+}
